703536598203131c0d9743cf6d370cb243b3fa94
ducdh-fabbi <ducdh@fabbi.com.vn>
2020-11-26 09:49:52 +0700

    change fast image

--- AnimatableImage.js
+++ AnimatableImage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Image, Animated } from 'react-native';
+import FastImage from 'react-native-fast-image';
+const AnimatedFastImage = Animated.createAnimatedComponent(FastImage);
 
 function AnimatableImage(props) {
   const { animated, ...rest } = props;
 
-  const ImageComponent = animated ? Animated.Image : Image;
+  const ImageComponent = animated ? AnimatedFastImage : FastImage;
 
   return <ImageComponent {...rest} />;
 }
